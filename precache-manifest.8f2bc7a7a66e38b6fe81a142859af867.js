7cfd6e7ac04ff759156385a1f84201afcd4196d3
Indranil Bit <indranil.bitzxcvbnm@gmail.com>
2022-03-19 12:26:50 +0530

    Portfolio serve

---
+++ precache-manifest.8f2bc7a7a66e38b6fe81a142859af867.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b49731e5f514b82058e390e89f066b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a13ff5f2b3b9df334f95",
+    "url": "/static/css/main.e8fa8d02.chunk.css"
+  },
+  {
+    "revision": "2cdb02873bcf32176e0a",
+    "url": "/static/js/2.f4fe423a.chunk.js"
+  },
+  {
+    "revision": "bc050789b83d3fa8c8a1fa1924cd865e",
+    "url": "/static/js/2.f4fe423a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a13ff5f2b3b9df334f95",
+    "url": "/static/js/main.797248b8.chunk.js"
+  },
+  {
+    "revision": "0900b8459eff7f341ab9",
+    "url": "/static/js/runtime-main.9d604abd.js"
+  },
+  {
+    "revision": "afc0ab671791d5c593de29562745dd57",
+    "url": "/static/media/certificate.afc0ab67.svg"
+  },
+  {
+    "revision": "ecde08fbae4d4ff712a991f161a736e2",
+    "url": "/static/media/dp01.ecde08fb.jpg"
+  },
+  {
+    "revision": "f423a3af5abac02a65af1aba363462e5",
+    "url": "/static/media/project.f423a3af.svg"
+  }
+]);
